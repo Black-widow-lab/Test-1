1327320f60d0ef3e50b806e207dd9b29ca388c8e
Black-widow-lab <b03757166@gmail.com>
2026-06-03 16:26:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+async function sendCommand(command){
+
+try{
+
+```
+const response = await fetch('/api/sendcommand',{
+
+  method:'POST',
+
+  headers:{
+    'Content-Type':'application/json'
+  },
+
+  body:JSON.stringify({
+    command:command
+  })
+
+});
+
+const data = await response.json();
+
+console.log(data);
+```
+
+}
+catch(error){
+
+```
+console.error(error);
+```
+
+}
+
+}
+
 const picker1 = new iro.ColorPicker("#picker1",{
 
 width:220,
@@ -16,71 +51,122 @@ color:"#00ff00"
 
 picker1.on("color:change",function(color){
 
-console.log(
-"RGB1:",
-color.hexString
-);
+console.log("RGB1:",color.hexString);
 
 });
 
 picker2.on("color:change",function(color){
 
-console.log(
-"RGB2:",
-color.hexString
-);
+console.log("RGB2:",color.hexString);
 
 });
 
-document
-.querySelectorAll("input")
-.forEach(toggle=>{
+document.getElementById("led1").addEventListener("change",function(){
 
-toggle.addEventListener(
-"change",
-()=>{
+if(this.checked){
 
-console.log(
-toggle.id,
-toggle.checked
-?
-"ON"
-:
-"OFF"
-);
+```
+sendCommand("led1on");
+```
 
-});
+}else{
+
+```
+sendCommand("led1off");
+```
+
+}
 
 });
 
-document
-.getElementById("allOn")
-.addEventListener(
-"click",
-()=>{
+document.getElementById("led2").addEventListener("change",function(){
 
-document
-.querySelectorAll(
-'input[type="checkbox"]'
-)
-.forEach(
-t=>t.checked=true
-);
+if(this.checked){
+
+```
+sendCommand("led2on");
+```
+
+}else{
+
+```
+sendCommand("led2off");
+```
+
+}
 
 });
 
-document
-.getElementById("allOff")
-.addEventListener(
-"click",
-()=>{
+document.getElementById("led3").addEventListener("change",function(){
+
+if(this.checked){
+
+```
+sendCommand("led3on");
+```
+
+}else{
+
+```
+sendCommand("led3off");
+```
+
+}
+
+});
+
+document.getElementById("led4").addEventListener("change",function(){
+
+if(this.checked){
+
+```
+sendCommand("led4on");
+```
+
+}else{
+
+```
+sendCommand("led4off");
+```
+
+}
+
+});
+
+document.getElementById("led5").addEventListener("change",function(){
+
+if(this.checked){
+
+```
+sendCommand("led5on");
+```
+
+}else{
+
+```
+sendCommand("led5off");
+```
+
+}
+
+});
+
+document.getElementById("allOn").addEventListener("click",()=>{
 
 document
-.querySelectorAll(
-'input[type="checkbox"]'
-)
-.forEach(
-t=>t.checked=false
-);
+.querySelectorAll('input[type="checkbox"]')
+.forEach(t=>t.checked=true);
+
+sendCommand("allledson");
+
+});
+
+document.getElementById("allOff").addEventListener("click",()=>{
+
+document
+.querySelectorAll('input[type="checkbox"]')
+.forEach(t=>t.checked=false);
+
+sendCommand("allledsoff");
 
 });
